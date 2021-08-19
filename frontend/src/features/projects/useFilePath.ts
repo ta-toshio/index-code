4d0970e5c021b29a05e37663a11215b09712e1bb
ta-toshio <ta.toshio.info@gmail.com>
2021-08-19 18:38:53 +0900

    Extract function

--- frontend/src/features/projects/useFilePath.ts
+++ frontend/src/features/projects/useFilePath.ts
@@ -1,40 +1,14 @@
+import { useEffect, useMemo, useState } from 'react'
 import { useLazyQuery } from '@apollo/client'
 import { GET_ALL_FILE_PATH } from '../../queries/file'
 import {
   GetAllFilePathQuery,
   GetAllFilePathQueryVariables,
 } from '../../generated/graphql'
-import { useEffect, useMemo, useState } from 'react'
-
-const makeTree = (allFilePath, filePath, depth: number) => {
-  if (!filePath.is_dir) {
-    return {
-      key: filePath.file_path,
-      label: filePath.name,
-    }
-  }
-
-  const childNodes = allFilePath.filter(
-    (_filePath) => _filePath.parent_id == filePath.id
-  )
-  if (!childNodes.length) {
-    return {
-      key: filePath.file_path,
-      label: filePath.name,
-    }
-  }
-
-  return {
-    key: filePath.file_path,
-    label: filePath.name,
-    nodes: childNodes.map((_filePath) =>
-      makeTree(allFilePath, _filePath, depth + 1)
-    ),
-  }
-}
+import { makeTree } from '../../utils/tree'
 
 const useFilePath = ({ projectId }) => {
-  const [page, setPage] = useState<number>(1)
+  const [page] = useState<number>(1)
   const [fetchFilePath, { called, loading, data }] = useLazyQuery<
     GetAllFilePathQuery,
     GetAllFilePathQueryVariables
@@ -47,14 +21,14 @@ const useFilePath = ({ projectId }) => {
   }, [fetchFilePath, page, projectId])
 
   const tree = useMemo(() => {
-    if (data && data.getAllFilePath.data) {
-      return data.getAllFilePath.data
-        .filter((filePath) => filePath.depth === 0)
-        .map((filePath) => {
-          return makeTree(data.getAllFilePath.data, filePath, 0)
-        })
+    if (!data || !data.getAllFilePath.data) {
+      return []
     }
-    return []
+    return data.getAllFilePath.data
+      .filter((filePath) => filePath.depth === 0)
+      .map((filePath) => {
+        return makeTree(data.getAllFilePath.data, filePath, 0)
+      })
   }, [data])
 
   return {
